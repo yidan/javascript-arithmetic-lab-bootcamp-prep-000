711ee04d02c5f25911691a2599739cb2f9e4fe82
Yidan Wang <wangyidan621@gmail.com>
2017-10-31 00:34:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,16 @@ function divide(a, b){
 function inc(n){
   return n++;
 }
+
+
+function dec(n){
+  return n--;
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n);
+}
